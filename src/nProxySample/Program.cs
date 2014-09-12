92318985b37219c27b8fb1a9110a8caa60685201
Noel <noelanderton@cox.net>
2014-09-11 17:01:16 -0700

    console handling

--- src/nProxySample/Program.cs
+++ src/nProxySample/Program.cs
@@ -1,16 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Phantom Coder">
+//   
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace nProxySample
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Program
     {
+        static bool interrupted = false;
+
+        static void ConsoleCancelHandler(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            interrupted = true;
+        }
+
         static void Main(string[] args)
         {
+            var input = string.Empty;
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(ConsoleCancelHandler);
 
+            RunNProxyTest(input);
+        }
+
+        private static string RunNProxyTest(string input)
+        {
+            Console.WriteLine("enter to exit=>");
+            input = Console.ReadLine();
+
+            return input;
         }
     }
 }
