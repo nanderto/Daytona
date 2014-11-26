bc4e3846813abe6030f0bcfe8092c830f737fe47
Noel <noelanderton@cox.net>
2014-11-26 12:07:33 -0700

    Comments to better understand code

--- src/Store/Connection.cs
+++ src/Store/Connection.cs
@@ -6,6 +6,9 @@
     using System.Threading.Tasks;
     using ZeroMQ;
 
+    /// <summary>
+    /// Connection provides access to data persistence
+    /// </summary>
     public class Connection : IDisposable
     {
         private readonly Dictionary<string, IScope> scopes = new Dictionary<string, IScope>();
@@ -35,6 +38,11 @@
             return await result;
         }
 
+        /// <summary>
+        /// Adding a newscope that defines the access
+        /// </summary>
+        /// <typeparam name="T">THe type of object to be persisted</typeparam>
+        /// <param name="scope">the scope parameter of the specified type</param>
         internal void AddScope<T>(Scope<T> scope)
         {
             this.scopes.Add(typeof(T).Name, (IScope)scope);
