c161a9a6f2cfaf49aec57eb46bcd4e638863e38e
Noel <noelanderton@cox.net>
2014-11-30 11:27:39 -0700

    Commit to switch branches good ole git

---
+++ src/Silo/Program.cs
@@ -0,0 +1,64 @@
+﻿using Daytona;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHelpers;
+using ZeroMQ;
+
+namespace Silo
+{
+    using System.IO;
+    using System.Reflection;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(ConsoleCancelHandler);
+            var binarySerializer = new BinarySerializer();
+            var useActor = true;
+
+            using (var context = ZmqContext.Create())
+            {
+                using (var actorFactory = new Actor<Daytona.Silo>(context, new BinarySerializer(), string.Empty))
+                {
+                    actorFactory.RegisterActor(
+                        "Silo",
+                        "",
+                        "outRoute",
+                        new BinarySerializer(),
+                        (address, parameters, actor) =>
+                            {
+                                var firstParameter = string.Empty;
+                                try
+                                {
+                                    firstParameter = parameters[0].ToString();
+                                }
+                                catch (Exception)
+                                {
+                                }
+
+                                Console.WriteLine("Address: {0}, {1}", address, firstParameter);
+                            });
+                    actorFactory.StartAllActors();
+                    Console.WriteLine("yada yada");
+
+                    while (!interrupted)
+                    {
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+
+        static bool interrupted = false;
+
+        static void ConsoleCancelHandler(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            interrupted = true;
+        }
+    }
+}
